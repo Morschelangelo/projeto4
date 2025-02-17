b1c4a2d0e5704d6c66405ce69624f1ed54bfbf0b
Vincent <164825800+Morschelangelo@users.noreply.github.com>
2025-02-17 19:56:45 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,32 @@
-// Links para plugins e recursos
+// links para plugins e recursos
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // Certifique-se de usar o Dart Sass
+const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
-// Função para minificar scripts
+// configuração de funções
 function scripts() {
     return gulp.src('./src/scripts/*.js')
-        .pipe(uglify()) // Minifica os scripts
-        .pipe(gulp.dest('./public/js')); // Salva os arquivos minificados no diretório de destino "public"
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
 }
 
-// Função para compilar Sass
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Compila e comprime o Sass
-        .pipe(gulp.dest('./public/css')); // Salva os arquivos compilados no diretório "public"
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-// Função para otimizar imagens
 function images() {
     return gulp.src('./src/images/**/*')
-        .pipe(imagemin()) // Otimiza as imagens
-        .pipe(gulp.dest('./public/images')); // Salva as imagens otimizadas no diretório "public"
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-// Comando de exportação padrão
-exports.default = gulp.parallel(styles, images, scripts); // Compila Sass, otimiza imagens e minifica scripts
+// Configuração de comandos de exportação
+exports.default = gulp.parallel(styles, images, scripts);
 
-// Comando de observação (watch)
 exports.watch = function() {
-    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles)); // Observa mudanças nos arquivos .scss
-    gulp.watch('./src/scripts/**/*.js', gulp.parallel(scripts)); // Observa mudanças nos arquivos .js
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
